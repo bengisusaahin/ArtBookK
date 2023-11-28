2cb4f20db203fe09a69b6b4103fff06f681d00fe
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-28 20:35:23 +0300

    Created the menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bengisusahin.artbookk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bengisusahin.artbookk"
--- app/src/main/java/com/bengisusahin/artbookk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/artbookk/MainActivity.kt
@@ -2,6 +2,9 @@ package com.bengisusahin.artbookk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
 import com.bengisusahin.artbookk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_art_idem){
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
