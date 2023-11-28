78639c5ae429126693c202572d416f9aa16281ff
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-28 22:17:43 +0300

    Permission control

--- app/src/main/java/com/bengisusahin/artbookk/ArtActivity.kt
+++ app/src/main/java/com/bengisusahin/artbookk/ArtActivity.kt
@@ -1,9 +1,16 @@
 package com.bengisusahin.artbookk
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bengisusahin.artbookk.databinding.ActivityArtBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArtActivity : AppCompatActivity() {
 
@@ -19,7 +26,23 @@ class ArtActivity : AppCompatActivity() {
     }
 
     fun selectImage(view: View){
+        //read external izni dangerous level oldugu icin manifestte kontrol etmek yeterli degil
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //rationale gösterdikten sonra request permission buna karar veren androidin kendisi
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Give permission", View.OnClickListener {
+                        //request permission
+                    }).show()
+            }else{
+                //rationale göstermeden direkt request permission
+            }
 
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //intent
+        }
     }
 
 }
