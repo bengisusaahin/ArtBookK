c3fa62b9b5e8ec441fbca0c83c575dc884bf010d
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-01 16:07:36 +0300

    pull the data

---
+++ app/src/main/java/com/bengisusahin/artbookk/Art.kt
@@ -0,0 +1,4 @@
+package com.bengisusahin.artbookk
+
+class Art (val name: String, val id : Int) {
+}
--- app/src/main/java/com/bengisusahin/artbookk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/artbookk/MainActivity.kt
@@ -5,15 +5,42 @@ import android.os.Bundle
 import android.content.Intent
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bengisusahin.artbookk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var artList: ArrayList<Art>
+    private lateinit var artAdapter : ArtAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        artList = ArrayList<Art>()
+
+        artAdapter = ArtAdapter(artList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = artAdapter
+
+        try {
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+            val cursor = database.rawQuery("SELECT * FROM arts", null)
+            val artNameIx = cursor.getColumnIndex("artname")
+            val idIx = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()){
+                val name = cursor.getString(artNameIx)
+                val id = cursor.getInt(idIx)
+                val art = Art(name,id)
+                artList.add(art)
+            }
+            artAdapter.notifyDataSetChanged()
+            cursor.close()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
